43f19add2a70b27e0db2c57358276e06e51ccb37
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2021-01-31 04:22:04 +0900

    Better Errors, Bullet の設定 (#9)
    * add rubocop & fix with rubocop
    
    * add rspec
    
    * add -rspec-configuration
    
    * add -factory_bot_rails-configuration
    
    * add -better_errors-configuration
    
    * add -bullet-configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Bullet
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
+
+  # Better Errors for docker container
+  BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
 end
